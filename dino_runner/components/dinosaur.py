05e227aab3093e3dffe861ff633a170b729a188e
Roberto Cavalcanti <rickfreitas2001@gmail.com>
2023-05-22 13:06:41 -0300

    Método Duck implementado

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
@@ -16,6 +16,7 @@ class Dinosaur:
 
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.step_index = 0
         self.jump_vel = JUMP_VEL
 
@@ -41,17 +42,36 @@ class Dinosaur:
             self.dino_rect.y =  Y_POS
             self.jump_vel = JUMP_VEL
 
+    def duck(self):
+        self.dino_duck = False
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.y = Y_POS + 35
+        self.dino_rect.x = X_POS
+        self.step_index += 1
+
+        if self.step_index >=10:
+            self.step_index = 0
+        
+
     def update(self, user_input):
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if (user_input[pygame.K_UP] or user_input[pygame.K_w]) and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_jump:
+        
+        elif user_input[pygame.K_DOWN] or user_input[pygame.K_s]:
+            self.dino_duck = True 
+            self.dino_run = False
+        
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_run = True
 
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
         
     
 
