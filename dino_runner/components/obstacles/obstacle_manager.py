0b09888c59bb62df71a23b4610319a7452b5e7b0
Roberto Cavalcanti <rickfreitas2001@gmail.com>
2023-05-24 14:11:41 -0300

    correção final

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -2,16 +2,17 @@ import pygame
 import random
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.bird import Bird
-# from dino_runner.components.power_ups import power_up,hammer,shield
 from dino_runner.utils.constants import SMALL_CACTUS, SMALL_CACTUS_Y_POS, LARGE_CACTUS, LARGE_CACTUS_Y_POS, BIRD, HAMMER_TYPE, SHIELD_TYPE
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
+        self.random_obs = 0
 
     def update(self, game):
-        random_obs = random.randint(0,2)
         if len(self.obstacles) == 0:
+            random_obs = random.randint(0,2)
+            self.random_obs = random_obs
             if random_obs == 0:
                 self.obstacles.append(Cactus(SMALL_CACTUS, SMALL_CACTUS_Y_POS))
             elif random_obs == 1:
@@ -23,13 +24,12 @@ class ObstacleManager:
             obstacle.update(game.game_speed, self.obstacles)
 
             if game.player.dino_rect.colliderect(obstacle.rect):
-                if (game.player.type == SHIELD_TYPE and not random_obs == 2) or (game.player.type == HAMMER_TYPE and random_obs == 2)  or not game.player.has_power_up:
+                if (game.player.type == SHIELD_TYPE and not self.random_obs == 2) or (game.player.type == HAMMER_TYPE and self.random_obs == 2)  or not game.player.has_power_up:
                     pygame.time.delay(500)
                     game.playing = False
                     game.death_count += 1
                     break
                 else:
-                    # print(game.player.type)
                     self.obstacles.remove(obstacle)
 
     def draw(self, screen):
